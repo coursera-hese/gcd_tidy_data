2056f8175af536cb6a99d583da7d7d3d6377c867
Helgar Seyringer <Helgar.Seyringer@v-research.at>
2019-01-14 13:47:49 +0100

    add column-filter for features mean() and std()

--- run_analysis.R
+++ run_analysis.R
@@ -12,37 +12,49 @@ dataDir <- "./data/UCI HAR Dataset/"
 testDir <- paste0(dataDir, "test/")
 trainDir <- paste0(dataDir, "train/")
 
+## WRONG Attempt `read.csv` (sep=" "): reads in wrong number of columns, since it does not skip multiple whitespaces (e.g. "  ").
+##     Used `read.table` to fix this problem.
+# dfTestX <- read.csv(paste0(testDir, "X_test.txt"), header = FALSE, sep = " ") # wrong number of columns
+
 ## from data-directory
-dfActivityLabels <- read.csv(paste0(dataDir, "activity_labels.txt"), header = FALSE, sep = " ", col.names = c("id", "activity"))
-dfFeatures <-  read.csv(paste0(dataDir, "features.txt"), header = FALSE, sep = " ", col.names = c("id", "feature"))
+#dfActivityLabels <- read.csv(paste0(dataDir, "activity_labels.txt"), header = FALSE, sep = " ", col.names = c("id", "activity"))
+#dfFeatures <-  read.csv(paste0(dataDir, "features.txt"), header = FALSE, sep = " ", col.names = c("id", "feature"))
+dfActivityLabels <- read.table(paste0(dataDir, "activity_labels.txt"), col.names = c("id", "activity"))
+dfFeatures <-  read.table(paste0(dataDir, "features.txt"), col.names = c("id", "feature"))
+
 ## from test-directory
-dfTestSub <- read.csv(paste0(testDir, "subject_test.txt"), header = FALSE, sep = " ", col.names = c("subject"))
-# WRONG Attempt `read.csv` (sep=" "): reads in wrong number of columns, since it does not skip multiple whitespaces (e.g. "  ").
-#dfTestX <- read.csv(paste0(testDir, "X_test.txt"), header = FALSE, sep = " ") # wrong number of columns
+dfTestSub <- read.table(paste0(testDir, "subject_test.txt"), col.names = c("subject"))
 dfTestX <- read.table(paste0(testDir, "X_test.txt"))
-dfTestY <- read.csv(paste0(testDir, "y_test.txt"), header = FALSE, sep = " ", col.names = c("activity"))
+dfTestY <- read.table(paste0(testDir, "y_test.txt"), col.names = c("activity"))
 ## from train-directory
-dfTrainSub <- read.csv(paste0(trainDir, "subject_train.txt"), header = FALSE, sep = " ", col.names = c("subject"))
+dfTrainSub <- read.table(paste0(trainDir, "subject_train.txt"), col.names = c("subject"))
 dfTrainX <- read.table(paste0(trainDir, "X_train.txt"))
 dfTrainY <- read.table(paste0(trainDir, "y_train.txt"), col.names = c("activity"))
+#dfTestY <- read.csv(paste0(testDir, "y_test.txt"), header = FALSE, sep = " ", col.names = c("activity"))
 #dfTrainY <- read.csv(paste0(trainDir, "y_train.txt"), header = FALSE, sep = " ", col.names = c("activity"))
 
-rm(tmp)
-tmp <- read.table(paste0(testDir, "X_test.txt"))
-
-# merge datasets from train- and test
-#dfSubject <- merge(dfTestSub, dfTrainSub)
-# TODO
 
 #
 # prepare and merge data
 #
 
+# filter features
+# filter 'features' with regexp to find all columns containing 'mean()' and 'std()' in the name
+featureFilter <- grepl("(mean\\(\\)|std\\(\\))", dfFeatures[,2])
+columnIndex <- dfFeatures[featureFilter,1]
+columnNames <- dfFeatures[featureFilter,2]
+# filter by the calculated column-index
+dfTestX <- dfTestX[,columnIndex]
+dfTrainX <- dfTrainX[,columnIndex]
+# rename columns
+names(dfTestX) <- columnNames
+names(dfTrainX) <- columnNames
+
 # merge data (`cbind`, `rbind` ...)
-# bind columns together
+# append columns from Subject, TestY and TestX (bind columns together)
 dfTest <- cbind(dfTestSub, dfTestY, dfTestX)
 dfTrain <- cbind(dfTrainSub, dfTrainY, dfTrainX)
-# bind rows together
+# merge datasets test and train (bind rows together)
 df <- rbind(dfTest, dfTrain)
 
 ## columns to lower-case conversion
@@ -51,6 +63,7 @@ names(df) <- tolower(names(df))
 # function `activity` gets the activity-name for a given activity-Id
 activity <- function(x) { dfActivityLabels[x,2] }
 df$activity <- sapply(df$activity, activity)
-#dfTest$activity <- sapply(dfTest$activity, activity)
-#dfTrain$activity <- sapply(dfTrain$activity, activity)
 
+# information on the dataset
+print(paste("columns x rows:", ncol(df), "x", nrow(df)))
+print(object.size(df),units="Mb")
